312707feb97ed2872cdba39894cd94a887bda639
mikkelnrasmussen <s193518@dtu.dk>
2024-01-11 14:56:26 +0100

    analys: download FASTQ raw reads from SRA

--- src/rnaseq/01_download_raw_data.sh
+++ src/rnaseq/01_download_raw_data.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+# Accept file with accession numbers and output directory from the command line
+acc_list=$1
+out_dir=$2
+
+# Download all the raw FASTQ files from SRA in parallel
+while read l; do
+  echo $l
+  fastq-dump --outdir $out_dir "$l"
+done <$acc_list | head -n 1
