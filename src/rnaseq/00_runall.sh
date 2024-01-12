b6bc7f5a6925e22ed33a5f530a1a2ca6a73cd36c
mikkelnrasmussen <s193518@dtu.dk>
2024-01-12 12:40:03 +0100

    fix: specify the correct paths for log file

--- src/rnaseq/00_runall.sh
+++ src/rnaseq/00_runall.sh
@@ -1,7 +1,7 @@
 # Step 01: Download RNA-seq FASTQ raw reads from SRA
-( time ./src/preprocess/01_download_raw_data.sh \
-  -f data/rnaseq/rnaseq_acc_list_test.txt \
-  -d data/rnaseq/_raw/ \
-  -t single ) 2>&1 | tee logs/download_time_rnaseq.log
+( time ./src/preprocess/01_download_raw_data.sh\
+ -f data/rnaseq/rnaseq_acc_list.txt\ 
+ -d data/rnaseq/_raw/\ 
+ -t single ) 2>&1 | tee logs/rnaseq/download/download_time_rnaseq.log
 
 # Step 02: Run FASTQC on raw reads
