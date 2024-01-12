341db4a3f0208ed053c4eebeb18bbb53e1834e3b
thomas-tams-dtu <s204540@dtu.dk>
2024-01-12 11:42:42 +0000

    update: added gz compatability

--- src/amplicon/04_bbduk_trim.sh
+++ src/amplicon/04_bbduk_trim.sh
@@ -12,6 +12,6 @@ trimmed_singletons="../../data/amplicon/trimmed_reads/SRR1567776_trimmed_singlet
 trimmed_dir="../../data/amplicon/trimmed_reads"
 cd $trimmed_dir
 
-ls ../_raw/*_1.fastq | parallel 'base=$(basename {}); read2=$(echo {} | sed "s/_1/_2/"); trim1=${base%????????}_trimmed_1.fastq; trim2=${base%????????}_trimmed_2.fastq; st=${base%????????}_trimmed_singleton.fastq; bbduk.sh in1={} in2=$read2 out1=$trim1 out2=$trim2 outs=$st threads=10 k=25 kmin=11 ktrim=r qtrim=r trimq=20 minlength=50 overwrite=t ziplevel=6 ref=../adapters/adapters.fa'
+ls ../_raw/*_1.fastq.gz | parallel 'base=$(basename {}); read2=$(echo {} | sed "s/_1/_2/"); trim1=${base%???????????}_trimmed_1.fastq.gz; trim2=${base%???????????}_trimmed_2.fastq.gz; st=${base%???????????}_trimmed_singleton.fastq.gz; bbduk.sh -Xmx4g in1={} in2=$read2 out1=$trim1 out2=$trim2 outs=$st threads=10 k=25 kmin=11 ktrim=r qtrim=r trimq=20 minlength=50 overwrite=t ziplevel=6 ref=../adapters/adapters.fa tbo tpe'
 
 #threads=10 k=25 kmin=11 ktrim=r qtrim=r trimq=20 minlength=50 overwrite=t ziplevel=6 ref=../adapters/adapters.fa"
--- src/amplicon/05_fastqc_trimmed.sh
+++ src/amplicon/05_fastqc_trimmed.sh
@@ -4,5 +4,5 @@ output_dir="/net/pupil1/home/projects/22126_NGS/projects/group8/data/amplicon/fa
 log_path="../../logs/amplicon/fastqc_trimmed.log"
 
 # Use parallel to process lines concurrently
-ls ../../data/amplicon/trimmed_reads/*_1.fastq | parallel fastqc -q -o $output_dir {} > $log_path
-ls ../../data/amplicon/trimmed_reads/*_2.fastq | parallel fastqc -q -o $output_dir {} >> $log_path
+ls ../../data/amplicon/trimmed_reads/*_1.fastq.gz | parallel fastqc -q -o $output_dir {} > $log_path
+ls ../../data/amplicon/trimmed_reads/*_2.fastq.gz | parallel fastqc -q -o $output_dir {} >> $log_path
