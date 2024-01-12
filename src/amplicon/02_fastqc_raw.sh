12eaf16ff40bd88219a04c71ad9d9fea9cb325e4
mikkelnrasmussen <s193518@dtu.dk>
2024-01-12 13:27:04 +0100

    build: remove amplicon specific download and fastqc

--- src/amplicon/02_fastqc_raw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-output_dir="/net/pupil1/home/projects/22126_NGS/projects/group8/data/amplicon/fastqc_raw"
-log_path="../../logs/amplicon/fastqc_raw.log"
-
-# Use parallel to process lines concurrently
-ls ../../data/amplicon/_raw/*.fastq | parallel fastqc -q -o $output_dir {} > $log_path
