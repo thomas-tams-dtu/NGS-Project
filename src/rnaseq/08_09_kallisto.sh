8620ae2fb858f5f2c868a26b1400231c3afd80de
mikkelnrasmussen <s193518@dtu.dk>
2024-01-12 17:07:52 +0100

    feat: add command line args and run in parallel

--- src/rnaseq/08_09_kallisto.sh
+++ src/rnaseq/08_09_kallisto.sh
@@ -3,30 +3,59 @@
 # Define kallisto as path
 kallisto="/home/ctools/kallisto/build/src/kallisto"
 
-# Define path to the reference transcriptome
-REFERENCE_TRANSCRIPTOME="/home/projects/22126_NGS/projects/group8/data/references/GRCh38_latest_rna.fna.gz"
+# Default values for command-line arguments
+REFERENCE_TRANSCRIPTOME="data/references/GRCh38_latest_rna.fna.gz"
+READ_DIR=""
+OUTPUT_DIR_ROOT=""
+NUM_THREADS=4  # Default number of threads
+
+# Function to show usage
+usage() {
+    echo "Usage: $0 -f <reference_transcriptome> -r <read_dir> -o <output_dir_root> -t <num_threads>"
+    echo "  -f  Path to the reference transcriptome"
+    echo "  -r  Directory containing trimmed read files"
+    echo "  -o  Root directory for Kallisto output"
+    echo "  -t  Number of threads for Kallisto (default: 4)"
+    exit 1
+}
+
+# Parse command-line options
+while getopts 'f:r:o:t:' flag; do
+    case "${flag}" in
+        f) REFERENCE_TRANSCRIPTOME=${OPTARG} ;;
+        r) READ_DIR=${OPTARG} ;;
+        o) OUTPUT_DIR_ROOT=${OPTARG} ;;
+        t) NUM_THREADS=${OPTARG} ;;
+        *) usage ;;
+    esac
+done
+
+# Check if all required options are provided
+if [ -z "$REFERENCE_TRANSCRIPTOME" ] || [ -z "$READ_DIR" ] || [ -z "$OUTPUT_DIR_ROOT" ]; then
+    usage
+fi
 
 # Create Kallisto index (only needs to be done once)
-kallisto index -i /home/projects/22126_NGS/projects/group8/data/references/kallisto_human_index.idx $REFERENCE_TRANSCRIPTOME
+$kallisto index -i "${OUTPUT_DIR_ROOT}/kallisto_human_index.idx" "$REFERENCE_TRANSCRIPTOME"
 
-# Directory containing your read files
-READ_DIR="/home/projects/22126_NGS/projects/group8/data/rnaseq/trimmed"
+# Export variables for use in the exported function
+export OUTPUT_DIR_ROOT kallisto
 
-# Create an array of all fastq.gz files in the specified directory
-READ_FILES=($(ls $READ_DIR/*.fastq.gz))
-
-# Loop over the array and run Kallisto quant for each file
-for READ_FILE in "${READ_FILES[@]}"
-do
-    # Extract the base name of the file for output directory
-    BASE_NAME=$(basename $READ_FILE .fastq.gz)
-    OUTPUT_DIR="home/projects/22126_NGS/projects/group8/data/rnaseq/kallisto/output_dir_${BASE_NAME}"
+# Kallisto quant command function
+kallisto_quant() {
+    local read_file=$1
+    local base_name=$(basename "$read_file" .fastq.gz)
+    local output_dir="${OUTPUT_DIR_ROOT}/${base_name}"
 
     # Create the output directory if it doesn't exist
-    mkdir -p $OUTPUT_DIR
+    mkdir -p "$output_dir"
 
     # Run Kallisto quant
-    kallisto quant -i /home/projects/22126_NGS/projects/group8/data/references/kallisto_human_index.idx -o $OUTPUT_DIR --single -l 50 -s 5 $READ_FILE
-    # Note: -l and -s are the estimated average fragment length and standard deviation, respectively.
-    # Adjust these values based on your experimental setup.
-done
+    nice -n 19 $kallisto quant -i "${OUTPUT_DIR_ROOT}/kallisto_human_index.idx" -o "$output_dir" -b 100 --single -l 50 -s 5 -t "$NUM_THREADS" "$read_file"
+}
+
+# Export the function for parallel to use
+export -f kallisto_quant
+
+# Find all fastq.gz files and run them in parallel
+find "$READ_DIR" -name '*.fastq.gz' | head -n 1 | parallel -j "$NUM_THREADS" kallisto_quant {}
