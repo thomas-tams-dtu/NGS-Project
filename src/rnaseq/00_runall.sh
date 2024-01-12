45697935387606ff5feb02d19abd75e7e14417e8
mikkelnrasmussen <s193518@dtu.dk>
2024-01-12 13:26:04 +0100

    analys: add fastqc step for raw reads

--- src/rnaseq/00_runall.sh
+++ src/rnaseq/00_runall.sh
@@ -2,6 +2,12 @@
 ( time ./src/preprocess/01_download_raw_data.sh\
  -f data/rnaseq/rnaseq_acc_list.txt\ 
  -d data/rnaseq/_raw/\ 
- -t single ) 2>&1 | tee logs/rnaseq/download/download_time_rnaseq.log
+ -t single ) 2>&1 | tee logs/rnaseq/download/download_fastq_time.log
 
 # Step 02: Run FASTQC on raw reads
+( time ./src/preprocess/02_fastqc.sh\
+  -i data/rnaseq/_raw/\
+  -o results/rnaseq/fastqc/raw/\
+  -l logs/rnaseq/fastqc/fastqc_raw.log ) 2>&1 | tee logs/rnaseq/fastqc/fastqc_raw_time.log
+
+# Step 03: Trim reads with BBDduk
