839c8f28ad7d48fdbedd7b3b81eebf4d58bb3825
mikkelnrasmussen <s193518@dtu.dk>
2024-01-12 11:30:38 +0100

    analys: update step 1 with paths

--- src/rnaseq/00_runall.sh
+++ src/rnaseq/00_runall.sh
@@ -1,3 +1,8 @@
 # Step 01: Download RNA-seq FASTQ raw reads from SRA
-./src/rnaseq/01_download_raw_data.sh
+./src/preprocess/01_download_raw_data.sh \
+  -f /home/projects/22126_NGS/projects/group8/mikkel/NGS-Project/data/rnaseq/rnaseq_acc_list.txt \
+  -d data/rnaseq/_raw/ \
+  -t single
 
+
+# Step 02: Run FASTQC on raw reads
