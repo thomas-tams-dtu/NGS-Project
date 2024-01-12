198f6233c6e64f08115cd859a2fb61eacadb84d3
mikkelnrasmussen <s193518@dtu.dk>
2024-01-12 12:09:02 +0100

    fix: add timming of the download process

--- src/rnaseq/00_runall.sh
+++ src/rnaseq/00_runall.sh
@@ -1,8 +1,7 @@
 # Step 01: Download RNA-seq FASTQ raw reads from SRA
-./src/preprocess/01_download_raw_data.sh \
-  -f /home/projects/22126_NGS/projects/group8/mikkel/NGS-Project/data/rnaseq/rnaseq_acc_list.txt \
+( time ./src/preprocess/01_download_raw_data.sh \
+  -f data/rnaseq/rnaseq_acc_list_test.txt \
   -d data/rnaseq/_raw/ \
-  -t single
-
+  -t single ) 2>&1 | tee logs/download_time_rnaseq.log
 
 # Step 02: Run FASTQC on raw reads
