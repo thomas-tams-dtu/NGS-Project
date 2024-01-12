8a0c2f7301d3ea38930ca62b2cd33d81fc81d39b
mikkelnrasmussen <s193518@dtu.dk>
2024-01-12 16:26:22 +0100

    analys: update trimming and fastqc commands

--- src/rnaseq/00_runall.sh
+++ src/rnaseq/00_runall.sh
@@ -1,13 +1,35 @@
-# Step 01: Download RNA-seq FASTQ raw reads from SRA
-( time ./src/preprocess/01_download_raw_data.sh\
- -f data/rnaseq/rnaseq_acc_list.txt\ 
- -d data/rnaseq/_raw/\ 
- -t single ) 2>&1 | tee logs/rnaseq/download/download_fastq_time.log
+########## Step 01: Download RNA-seq FASTQ raw reads from SRA ##########
+( time bash src/preprocess/01_download_raw_fastq.sh \
+-f data/rnaseq/rnaseq_acc_list.txt \
+-d data/rnaseq/_raw/ \
+-t single \
+-l logs/rnaseq/download/fastq-dump.log ) 2>&1 | tee logs/rnaseq/download/download_fastq_time.log
 
-# Step 02: Run FASTQC on raw reads
-( time ./src/preprocess/02_fastqc.sh\
-  -i data/rnaseq/_raw/\
-  -o results/rnaseq/fastqc/raw/\
-  -l logs/rnaseq/fastqc/fastqc_raw.log ) 2>&1 | tee logs/rnaseq/fastqc/fastqc_raw_time.log
+########## Step 02: Trim reads with BBDduk ##########
+( time bash src/preprocess/02_bbduk_trim.sh \
+-r data/rnaseq/_raw/ \
+-t data/rnaseq/trimmed/ \
+-a data/adapters/adapters.fa \
+-T single \
+-l logs/rnaseq/trim/bbduk.log \
+-p 10 ) 2>&1 | tee logs/rnaseq/trim/trim_bbduk_time.log
 
-# Step 03: Trim reads with BBDduk
+########## Step 03a: Run FASTQC on raw reads ##########
+( time bash src/preprocess/03_fastqc.sh \
+-i data/rnaseq/_raw/ \
+-o results/rnaseq/fastqc/raw/ \
+-l logs/rnaseq/fastqc/fastqc_raw.log ) 2>&1 | tee logs/rnaseq/fastqc/fastqc_raw_time.log
+
+########## Step 03b: Run FASTQC on trimmed reads ##########
+( time bash src/preprocess/03_fastqc.sh \
+-i data/rnaseq/trimmed/ \
+-o results/rnaseq/fastqc/trimmed/ \
+-l logs/rnaseq/fastqc/fastqc_trimmed.log ) 2>&1 | tee logs/rnaseq/fastqc/fastqc_trimmed_time.log
+
+########## Step 05: Run multiQC on raw reads ##########
+
+
+########## Step 06: Run multiQC on trimmed reads ##########
+
+
+
