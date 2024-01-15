97c60c04e6d26f4f222e1e561a0d9e0d5e4fc5fd
mikkelnrasmussen <s193518@dtu.dk>
2024-01-15 21:15:16 +0100

    analys: use for loop for memory heavy run

--- src/rnaseq/06_kallisto_alignment.sh
+++ src/rnaseq/06_kallisto_alignment.sh
@@ -7,51 +7,53 @@ kallisto="/home/ctools/kallisto/build/src/kallisto"
 READ_DIR=""
 OUTPUT_DIR_ROOT=""
 LOG_PATH=""
+NUM_THREADS=4  # Default number of threads
 
 # Function to show usage
 usage() {
-    echo "Usage: $0 -r <read_dir> -o <output_dir_root> -l <log_path>"
-    echo "  -r  Directory containing trimmed read files"
+    echo "Usage: $0 -r <read_dir> -o <output_dir_root> -l <log_path> -t <num_threads>"
+    echo "  -r  Directory containing fastq.gz read files"
     echo "  -o  Root directory for Kallisto output"
     echo "  -l  Log file path for Kallisto output"
+    echo "  -t  Number of threads for Kallisto (default: 4)"
     exit 1
 }
 
 # Parse command-line options
-while getopts 'r:o:l:' flag; do
+while getopts 'r:o:l:t:' flag; do
     case "${flag}" in
         r) READ_DIR=${OPTARG} ;;
         o) OUTPUT_DIR_ROOT=${OPTARG} ;;
         l) LOG_PATH=${OPTARG} ;;
+        t) NUM_THREADS=${OPTARG} ;;
         *) usage ;;
     esac
 done
 
 # Check if all required options are provided
-if 
-  [ -z "$READ_DIR" ] || [ -z "$OUTPUT_DIR_ROOT" ] || [ -z "$LOG_PATH" ]; then
-  usage
+if [ -z "$READ_DIR" ] || [ -z "$OUTPUT_DIR_ROOT" ] || [ -z "$LOG_PATH" ]; then
+    usage
 fi
 
-mkdir mapped
-#dir="rawdata/F23A490000016_HOMfpvqR/CleanData"
+# Export variables for use in the exported function
+export OUTPUT_DIR_ROOT NUM_THREADS kallisto
 
-# Define location of sample reads
-FOLDERS=($(find "$READ_DIR" -type d))
-FOLDERS=("${FOLDERS[@]:1}") # First element is just the $dir
-echo $FOLDERS
-# # Loop through the sample folders
-# for folder in "${FOLDERS[@]}"
-# do
-#     # Find all files in the current folder and sort them
-#     FILES=($(find "$folder" -type f | sort))
 
-#     # Run Kallisto for all files in the current folder
-#     if (( ${#FILES[@]} )); then # only run kallisto if there are files in the folder
-#         BASENAME=$(basename "$folder")
-#         base_name=$(basename "$folder" .fastq.gz)
-#         output_dir="${folder}/$(echo $base_name | sed "s/_trimmed//" )"
-#         echo "${FILES[@]}"
-#         nice -n 19 $kallisto quant -i "${OUTPUT_DIR_ROOT}/GRCh38.p13_index" -o mapped/"$BASENAME" --single -l 200 -s 20 "${FILES[@]}"
-#     fi
-# done
+# Kallisto quant command function
+kallisto_quant() {
+    local read_file=$1
+    local base_name=$(basename "$read_file" .fastq.gz)
+    local output_dir="${OUTPUT_DIR_ROOT}/$(echo $base_name | sed "s/_trimmed//" )"
+
+    # Create the output directory if it doesn't exist
+    mkdir -p "$output_dir"
+
+    # Run Kallisto quant and redirect output to log file
+    nice -19 $kallisto quant -i "${OUTPUT_DIR_ROOT}/GRCh38.p13_index" -o "$output_dir" --single -l 200 -s 20 -t "$NUM_THREADS" "$read_file"
+}
+
+# Export the function for parallel to use
+export -f kallisto_quant
+
+# Find all fastq.gz files and run them in parallel
+find "$READ_DIR" -name '*.fastq.gz' | parallel -j "$NUM_THREADS" kallisto_quant {} 2>&1 | tee -a "$LOG_PATH"
