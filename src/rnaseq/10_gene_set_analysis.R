35b78363b215fca5c5153bda0d4cc9a31da3aa77
mikkelnrasmussen <s193518@dtu.dk>
2024-01-16 17:51:38 +0100

    analys: add script for performing GSEA

---
+++ src/rnaseq/10_gene_set_analysis.R
@@ -0,0 +1,53 @@
+# Load libraries
+library("tidyverse")
+library("pairedGSEA")
+
+# Load DESeq2 and DEXSeq results
+aggregate_results <- readRDS("data/rnaseq/diff_exprs/Pediatric_Crohn_disease_aggregated_pvals.RDS")
+aggregate_results
+
+# Load gene sets
+gene_set_list <- readRDS("data/rnaseq/diff_exprs/gene_sets.RDS")
+
+# Run over-representation analysis
+paired_ora <- paired_ora(
+    paired_diff_result = aggregate_results,
+    gene_sets = gene_set_list,
+    experiment_title = "Pediatric_Crohn_disease"
+)
+
+### DGE:
+dge_ora_sorted <- paired_ora %>%
+  arrange(padj_deseq) %>%
+  dplyr::select(pathway, padj_deseq, enrichment_score_deseq)
+head(dge_ora_sorted, 20)
+
+### DGU ORA:
+dgu_ora_sorted <- paired_ora %>%
+  arrange(padj_dexseq) %>%
+  dplyr::select(pathway, padj_dexseq, enrichment_score_dexseq)
+head(dgu_ora_sorted, 15)
+
+
+ora_plot <- plot_ora(
+    ora = paired_ora,
+    plotly = FALSE,
+    pattern = "INTERFERON", # Identify all gene sets about XXXX
+    cutoff = 0.1, # Only include significant gene sets
+    lines = TRUE, # Guide lines
+    colors = c('red','blue','black')
+)
+ggsave("results/ora_plot_interferon.png", ora_plot)
+
+data(examplePathways)
+data(exampleRanks)
+## Not run:
+example_ranks <- aggregate_results %>%
+  drop_na(padj_deseq) %>%
+  pull(padj_deseq)
+names(example_ranks) <- aggregate_results %>%
+  drop_na(padj_deseq) %>%
+  pull(gene)
+
+enrichment_plot <- plotEnrichment(gene_set_list[["REACTOME_INTERFERON_GAMMA_SIGNALING"]], example_ranks)
+ggsave("results/enrichment_plot.png", enrichment_plot)
